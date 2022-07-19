bf3429f3a91e089e102b429d774d35e1756e0755
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-19 12:15:48 -0300

    criando a página 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
@@ -6,12 +9,11 @@ from .models import Question
 def index(request):
     # Exibe as últimas 5 perguntas da enquete no sistema:
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def results(request, question_id):
@@ -21,3 +23,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
